33ab73c52f4102620e4833633c367cafdb694f5e
Brian Lesko <brianjosephlesko@gmail.com>
2024-03-25 21:40:14 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 import imu
 from machine import Pin, I2C
 
+# PWM
 from machine import Pin, PWM, ADC
 a2 = Pin(27)
 # pin 21 is Pin D8      # pin 23 is Pin D7      # Pin 24 is D3      # Pin 27 is D9
@@ -13,6 +14,11 @@ pwm = PWM(a2)
 duty = 30000 # sets the duty cycle
 pwm.freq(50) # standard is 50 Hz for Servos
 
+# Serial communication
+from machine import UART
+uart = UART(0, 9600)
+
+# IMU
 bus = I2C(1, scl=Pin(15), sda=Pin(14))
 imu = imu.IMU(bus)
 
@@ -23,14 +29,31 @@ def set_servo_angle(angle):
     pwm.duty_u16(duty)
 
 while (True):
-    print('Accelerometer: x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}'.format(*imu.accel()))
-    print('Gyroscope:     x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}'.format(*imu.gyro()))
-    print('Magnetometer:  x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}'.format(*imu.magnet()))
-    print("")
-    time.sleep_ms(100)
     
-    set_servo_angle(0)  # Minimum angle
-    time.sleep_ms(300)
-    set_servo_angle(180)  # Maximum angle
-    time.sleep_ms(300)
-    #PWM(Pin(n), freq=1_000_000, period=1000)
+    if uart.any():
+        data = uart.readline()  # Read a line of data from the UART
+        print(data.decode())  # Decode byte data to string and print it
+        
+        if isinstance(data, (int, float)):
+            set_servo_angle(float(data)*180)
+            print(f"Set the servo angle to {data*180}")
+            print("")
+            
+        if isinstance(data,str):
+            print('Accelerometer: x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}'.format(*imu.accel()))
+            print('Gyroscope:     x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}'.format(*imu.gyro()))
+            print('Magnetometer:  x:{:>8.3f} y:{:>8.3f} z:{:>8.3f}'.format(*imu.magnet()))
+            print("")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
